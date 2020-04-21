6c47a7186fa1b210baf2afa2f5250984e03f1ce1
tvitas <vitalijus.trainys@gmail.com>
2020-04-21 10:58:53 +0300

    Init

---
+++ readme.md
@@ -0,0 +1,5 @@
+## Naive Logger
+Naive logger. A part of [cccf site project](https://github.com/tvitas/cccf "CCCF site On Github").
+
+### Basic usage
+To do.
---
+++ src/NaiveLogger.php
@@ -0,0 +1,42 @@
+<?php
+namespace tvitas\NaiveLogger;
+use Psr\Log\AbstractLogger;
+
+class NaiveLogger extends AbstractLogger
+{
+    private $logFile = '';
+
+    public function __construct($logFile = '')
+    {
+        ('' === $logFile) ? $this->logFile = __DIR__ . '/../log/naive.log' : $this->logFile = $logFile;
+    }
+
+    public function log($level, $message, array $context = array())
+    {
+        $dateTime = date('Y-m-d H:i:s');
+        $logMsg = $message;
+        if (!empty($context)) {
+            $logMsg = $this->interpolate($message, $context);
+        }
+        $logStr = $dateTime . ' ' . strtoupper($level) . ': ' . $logMsg . PHP_EOL;
+        $fileContent = $logStr;
+        if (file_exists($this->logFile)) {
+            $fileContent = file_get_contents($this->logFile) . $logStr;
+        }
+        file_put_contents($this->logFile, $fileContent);
+    }
+
+    private function interpolate($message, array $context)
+    {
+        // build a replacement array with braces around the context keys
+        $replace = array();
+        foreach ($context as $key => $val) {
+            // check that the value can be casted to string
+            if (!is_array($val) && (!is_object($val) || method_exists($val, '__toString'))) {
+                $replace['{' . $key . '}'] = $val;
+            }
+        }
+        // interpolate replacement values into the message and return
+        return strtr($message, $replace);
+    }
+}
